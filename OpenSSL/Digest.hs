dcbe78f0df7dee5cc66af7514f7dcccf0c45f533
Peter Simons <simons@cryp.to>
2014-12-17 18:14:30 +0100

    Add support for digest algorithms MDC2, SHA224, SHA256, SHA384, and SHA512.

--- OpenSSL/Digest.hs
+++ OpenSSL/Digest.hs
@@ -2,7 +2,7 @@
 
 {- |
    Module      :  OpenSSL.Digest
-   Copyright   :  (c) 2010 by Peter Simons
+   Copyright   :  (c) 2014 by Peter Simons
    License     :  BSD3
 
    Maintainer  :  simons@cryp.to
@@ -33,6 +33,11 @@
    > DSS:       5bcaff7f22ff533ca099b3408ead876c0ebba9a7
    > DSS1:      5bcaff7f22ff533ca099b3408ead876c0ebba9a7
    > RIPEMD160: bdb2bba6ec93bd566dc1181cadbc92176aa78382
+   > MDC2:      112db2200ce1e9db3c2d132aea4ef7d0
+   > SHA224:    1ee0f9d93a873a67fe781852d716cb3e5904e015aafaa4d1ff1a81bc
+   > SHA256:    41ef4bb0b23661e66301aac36066912dac037827b4ae63a7b1165a5aa93ed4eb
+   > SHA384:    ae2a5d6649035c00efe2bc1b5c97f4d5ff97fa2df06f273afa0231c425e8aff30e4cc1db5e5756e8d2245a1514ad1a2d
+   > SHA512:    8470cdd3bf1ef85d5f092bce5ae5af97ce50820481bf43b2413807fec37e2785b533a65d4c7d71695b141d81ebcd4b6c4def4284e6067f0b400000001b230205
 -}
 
 module OpenSSL.Digest where
@@ -52,9 +57,14 @@ data MessageDigest
   | MD5          -- ^ 128 bit
   | SHA          -- ^ 160 bit
   | SHA1         -- ^ 160 bit
-  | DSS          -- ^ 160 bit (SHA1)
-  | DSS1         -- ^ 160 bit (SHA1)
+  | DSS          -- ^ other name for SHA1
+  | DSS1         -- ^ other name for SHA1
   | RIPEMD160    -- ^ 160 bit
+  | MDC2         -- ^ 128 bit
+  | SHA224       -- ^ 224 bit
+  | SHA256       -- ^ 256 bit
+  | SHA384       -- ^ 384 bit
+  | SHA512       -- ^ 512 bit
   deriving (Show, Eq, Enum, Bounded)
 
 -- |A convenience wrapper which computes the given digest
@@ -183,13 +193,18 @@ foreign import ccall unsafe "EVP_DigestFinal" digestFinal ::
 
 -- ** Message Digest Engines
 
-foreign import ccall unsafe "EVP_md_null"   mdNull      :: IO MDEngine
-foreign import ccall unsafe "EVP_md5"       mdMD5       :: IO MDEngine
-foreign import ccall unsafe "EVP_sha"       mdSHA       :: IO MDEngine
-foreign import ccall unsafe "EVP_sha1"      mdSHA1      :: IO MDEngine
 foreign import ccall unsafe "EVP_dss"       mdDSS       :: IO MDEngine
 foreign import ccall unsafe "EVP_dss1"      mdDSS1      :: IO MDEngine
+foreign import ccall unsafe "EVP_md5"       mdMD5       :: IO MDEngine
+foreign import ccall unsafe "EVP_md_null"   mdNull      :: IO MDEngine
+foreign import ccall unsafe "EVP_mdc2"      mdMDC2      :: IO MDEngine
 foreign import ccall unsafe "EVP_ripemd160" mdRIPEMD160 :: IO MDEngine
+foreign import ccall unsafe "EVP_sha"       mdSHA       :: IO MDEngine
+foreign import ccall unsafe "EVP_sha1"      mdSHA1      :: IO MDEngine
+foreign import ccall unsafe "EVP_sha224"    mdSHA224    :: IO MDEngine
+foreign import ccall unsafe "EVP_sha256"    mdSHA256    :: IO MDEngine
+foreign import ccall unsafe "EVP_sha384"    mdSHA384    :: IO MDEngine
+foreign import ccall unsafe "EVP_sha512"    mdSHA512    :: IO MDEngine
 
 -- |Map a 'MessageDigest' type into the the corresponding
 -- 'MDEngine'.
@@ -202,6 +217,11 @@ toMDEngine SHA1      = mdSHA1
 toMDEngine DSS       = mdDSS
 toMDEngine DSS1      = mdDSS1
 toMDEngine RIPEMD160 = mdRIPEMD160
+toMDEngine MDC2      = mdMDC2
+toMDEngine SHA224    = mdSHA224
+toMDEngine SHA256    = mdSHA256
+toMDEngine SHA384    = mdSHA384
+toMDEngine SHA512    = mdSHA512
 
 -- * Helper Functions
 
