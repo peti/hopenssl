449c9b81c880fead4eb91ecc2ad544652260ae11
Peter Simons <simons@cryp.to>
2017-05-15 18:06:04 +0200

    Extend the documentation.

--- src/OpenSSL/Digest.hs
+++ src/OpenSSL/Digest.hs
@@ -5,34 +5,88 @@
    Stability:   provisional
    Portability: portable
 
-   This module proivdes a high-level API to the message digest algorithms found
-   in OpenSSL's @crypto@ library.
+   This module provides a generic high-level API to the message digest
+   algorithms found in OpenSSL's @crypto@ library. There are two functions of
+   particular interest: 'digestByName' and 'digest'. The former can be used to
+   retrieve a 'DigestDescription', i.e. an OpenSSL object that implements a
+   particular algorithm. That type can then be used to compute actual message
+   digests:
 
-   Here is a short example program which runs all available digests on a
-   string:
+   >>> import Data.ByteString.Char8 ( pack )
+   >>> digest (digestByName "md5") (pack "Hello, world.")
+   [8,10,239,131,155,149,250,207,115,236,89,147,117,233,45,71]
 
-   > example :: (Enum a) => [a] -> IO [String]
-   > example input = mapM hash [minBound .. maxBound]
-   >   where
-   >   hash f = fmap (fmt f) (digest f (toWord input))
-   >   fmt f  = shows f . (":    \t"++) . (>>=toHex)
-   >   toWord = map (toEnum . fromEnum)
+   Neat pretty-printing can be achieved by running @'concatMap' 'toHex'@ over
+   the @[Word8]@ fingerprint returned by 'digest':
 
-   And when called, the function prints:
+   >>> digest (digestByName "md5") (pack "Hello, world.") >>= toHex
+   "080aef839b95facf73ec599375e92d47"
+   >>> digest (digestByName "sha1") (pack "Hello, world.") >>= toHex
+   "2ae01472317d1935a84797ec1983ae243fc6aa28"
 
-   > *Digest> example "open sesame" >>= putStr . unlines
-   > Null:
-   > MD5:       54ef36ec71201fdf9d1423fd26f97f6b
-   > SHA:       2ccefef64c76ac0d42ca1657457977675890c42f
-   > SHA1:      5bcaff7f22ff533ca099b3408ead876c0ebba9a7
-   > DSS:       5bcaff7f22ff533ca099b3408ead876c0ebba9a7
-   > DSS1:      5bcaff7f22ff533ca099b3408ead876c0ebba9a7
-   > RIPEMD160: bdb2bba6ec93bd566dc1181cadbc92176aa78382
-   > MDC2:      112db2200ce1e9db3c2d132aea4ef7d0
-   > SHA224:    1ee0f9d93a873a67fe781852d716cb3e5904e015aafaa4d1ff1a81bc
-   > SHA256:    41ef4bb0b23661e66301aac36066912dac037827b4ae63a7b1165a5aa93ed4eb
-   > SHA384:    ae2a5d6649035c00efe2bc1b5c97f4d5ff97fa2df06f273afa0231c425e8aff30e4cc1db5e5756e8d2245a1514ad1a2d
-   > SHA512:    8470cdd3bf1ef85d5f092bce5ae5af97ce50820481bf43b2413807fec37e2785b533a65d4c7d71695b141d81ebcd4b6c4def4284e6067f0b400000001b230205
+   The precise set of available digest algorithms provided by OpenSSL depends
+   on the version of the library installed into the system, obviously, but it's
+   reasonable to expect the following algorithms to be present: MD5, RIPEMD160,
+   SHA1, SHA224, SHA256, SHA384, and SHA512. If an algorithm is not available,
+   'digestByName' will throw an 'DigestAlgorithmNotAvailableInOpenSSL'
+   exception. If you don't like exceptions, use the tamer 'digestByName''
+   variant:
+
+   >>> digestByName' "i bet this algorithm won't exist"
+   Nothing
+
+   'DigestDescription' is an instance of 'IsString', so with the proper GHC
+   extensions installed it's possible to simplify the call to 'digest' even
+   further:
+
+   >>> :set -XOverloadedStrings
+   >>> digest "sha256" (pack "this can be simplified further") >>= toHex
+   "727311383e8fcc55da1f3b5a0afb6051b39e9cae3a72c89df3f4b40ce45f0a9a"
+
+   Last but not least, 'digest' is actually a class method of 'Digestable',
+   which collects things we can compute digests of. The defaults are
+   conservative, i.e. we support all things that correspond roughly to C's
+   construct of "void pointer plus a length". @digest@ can use with any of the
+   following signatures:
+
+   >>> shape1 = digest :: DigestDescription -> (Ptr (),    CSize) -> [Word8]
+   >>> shape2 = digest :: DigestDescription -> (Ptr Word8, CSize) -> [Word8]
+   >>> shape3 = digest :: DigestDescription -> (Ptr Word8, CUInt) -> [Word8]
+   >>> shape4 = digest :: DigestDescription -> (Ptr (),    Int)   -> [Word8]
+   >>> shape5 = digest :: DigestDescription -> StrictByteString   -> [Word8]
+   >>> shape6 = digest :: DigestDescription -> LazyByteString     -> [Word8]
+
+   'StrictByteString' and 'LazyByteString' are also instances of 'IsString' and
+   therefore subject to implicit construction from string literals:
+
+   >>> shape5 "sha256" "hello" == shape6 "sha256" "hello"
+   True
+
+   What we /don't/ offer are overloaded 'digest' versions for 'String' or
+   'CString'. These types produce non-deterministic results for Unicode strings
+   because their exact binary representation depends on the system's locale.
+   For those who absolutely want to hash 'String', the following specialized
+   function is provided:
+
+   >>> digestString "md5" "no Digestable instance for this sucker" >>= toHex
+   "a74827f849005794565f83fbd68ad189"
+
+   If you don't mind orphaned instances, however, feel free to shoot yourself
+   in the foot:
+
+   >>> :set -XFlexibleInstances
+   >>> :{
+        instance Digestable String where
+          digestMany algo chunks = digest' algo $ \update ->
+            forM_ chunks $ \chunk ->
+              withCStringLen chunk $ \(ptr,len) ->
+                update (castPtr ptr, fromIntegral len)
+       :}
+
+   Now 'digest' works with 'String's:
+
+   >>> digest "sha256" ("now we can hash strings"::String) >>= toHex
+   "7f2989f173125810aa917c4ffe0e26ae1b5f7fb852274829c210297a43dfc7f9"
 -}
 
 module OpenSSL.Digest
