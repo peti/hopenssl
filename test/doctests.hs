f831635ec4241918617db42478d9e805cd52e8e6
Peter Simons <simons@cryp.to>
2017-05-22 18:24:49 +0200

    doctests: run doctests over hsc files, too

--- test/doctests.hs
+++ test/doctests.hs
@@ -9,4 +9,4 @@ main = do
   distDir <- fromMaybe "dist" `fmap` lookupEnv "HASKELL_DIST_DIR"
   let hscFilesDir = distDir ++ "/build"
       packageDB = distDir ++ "/package.conf.inplace"
-  doctest [ "-i" ++ hscFilesDir, "-package-db=" ++ packageDB, "-package=hopenssl", "src" ]
+  doctest [ "-package-db=" ++ packageDB, "-package=hopenssl", "src", hscFilesDir ]
