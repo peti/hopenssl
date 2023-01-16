273ab30d3bdf32a96f7a4fa9003384665c93533a
Peter Simons <simons@cryp.to>
2023-01-16 09:19:00 +0100

    Don't rely on RIPEMD160 in the test suite.
    OpenSSL 3.x has disabled RIPEMD160 in its default configuration.
    digestByName will find the algorithm, but attempts to actually
    use it fail. See https://github.com/openssl/openssl/issues/16994
    for details.

--- test/OpenSesame.hs
+++ test/OpenSesame.hs
@@ -3,7 +3,6 @@ module OpenSesame where
 opensesame :: [(String, String)]
 opensesame = [ ("MD5",       "54ef36ec71201fdf9d1423fd26f97f6b")
              , ("SHA1",      "5bcaff7f22ff533ca099b3408ead876c0ebba9a7")
-             , ("RIPEMD160", "bdb2bba6ec93bd566dc1181cadbc92176aa78382")
              , ("SHA224",    "1ee0f9d93a873a67fe781852d716cb3e5904e015aafaa4d1ff1a81bc")
              , ("SHA256",    "41ef4bb0b23661e66301aac36066912dac037827b4ae63a7b1165a5aa93ed4eb")
              , ("SHA384",    "ae2a5d6649035c00efe2bc1b5c97f4d5ff97fa2df06f273afa0231c425e8aff30e4cc1db5e5756e8d2245a1514ad1a2d")
