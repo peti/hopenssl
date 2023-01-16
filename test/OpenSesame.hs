7ccce9eace580bbddf9fb440e73e05bd7cb2bed0
Peter Simons <simons@cryp.to>
2023-01-16 09:16:14 +0100

    Remove obsolete digest algorithms from the "open sesame" test.
    OpenSSL 3.x has removed the digest names SHA, DSS, DSS1, and MDC2 from its
    default configuration, so we shouldn't rely on them in our test suite.

--- test/OpenSesame.hs
+++ test/OpenSesame.hs
@@ -2,12 +2,8 @@ module OpenSesame where
 
 opensesame :: [(String, String)]
 opensesame = [ ("MD5",       "54ef36ec71201fdf9d1423fd26f97f6b")
-             , ("SHA",       "2ccefef64c76ac0d42ca1657457977675890c42f")
              , ("SHA1",      "5bcaff7f22ff533ca099b3408ead876c0ebba9a7")
-             , ("DSS",       "5bcaff7f22ff533ca099b3408ead876c0ebba9a7")
-             , ("DSS1",      "5bcaff7f22ff533ca099b3408ead876c0ebba9a7")
              , ("RIPEMD160", "bdb2bba6ec93bd566dc1181cadbc92176aa78382")
-             , ("MDC2",      "112db2200ce1e9db3c2d132aea4ef7d0")
              , ("SHA224",    "1ee0f9d93a873a67fe781852d716cb3e5904e015aafaa4d1ff1a81bc")
              , ("SHA256",    "41ef4bb0b23661e66301aac36066912dac037827b4ae63a7b1165a5aa93ed4eb")
              , ("SHA384",    "ae2a5d6649035c00efe2bc1b5c97f4d5ff97fa2df06f273afa0231c425e8aff30e4cc1db5e5756e8d2245a1514ad1a2d")
